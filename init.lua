02b7d15785215bb72df273aa67f5ecd6fd01e121
Wuzzy <almikes@aol.com>
2016-12-28 23:23:10 +0100

    Version 1.0.0
    Hooray! Let's party!

--- init.lua
+++ init.lua
@@ -22,8 +22,8 @@ doc = {}
 
 -- Version number (follows the SemVer specification 2.0.0)
 doc.VERSION = {}
-doc.VERSION.MAJOR = 0
-doc.VERSION.MINOR = 11
+doc.VERSION.MAJOR = 1
+doc.VERSION.MINOR = 0
 doc.VERSION.PATCH = 0
 doc.VERSION.STRING = doc.VERSION.MAJOR.."."..doc.VERSION.MINOR.."."..doc.VERSION.PATCH
 
