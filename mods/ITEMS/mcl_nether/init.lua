73eee5b0636dff66ef9af5439f1ddaf54fbf236e
SmokeyDope <smokey@tilde.team>
2023-01-30 05:03:50 +0000

    Make ancient debris smeltable in a blast furnace
    Make netherite ingot crafting shapeless

--- mods/ITEMS/mcl_nether/init.lua
+++ mods/ITEMS/mcl_nether/init.lua
@@ -57,7 +57,7 @@ minetest.register_node("mcl_nether:ancient_debris", {
 	stack_max = 64,
 	tiles = {"mcl_nether_ancient_debris_top.png", "mcl_nether_ancient_debris_side.png"},
 	is_ground_content = true,
-	groups = {pickaxey=4, building_block=1, material_stone=1, xp=0},
+	groups = {pickaxey=4, building_block=1, material_stone=1, xp=0, blast_furnace_smeltable = 1},
 	drop = "mcl_nether:ancient_debris",
 	sounds = mcl_sounds.node_sound_stone_defaults(),
 	_mcl_blast_resistance = 1200,
@@ -405,12 +405,12 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
+		type = "shapeless",
 	output = "mcl_nether:netherite_ingot",
 	recipe = {
-		{"mcl_nether:netherite_scrap", "mcl_nether:netherite_scrap", "mcl_nether:netherite_scrap"},
-		{"mcl_nether:netherite_scrap", "mcl_core:gold_ingot", "mcl_core:gold_ingot"},
-		{"mcl_core:gold_ingot", "mcl_core:gold_ingot", ""},
-	}
+		"mcl_nether:netherite_scrap", "mcl_nether:netherite_scrap", "mcl_nether:netherite_scrap",
+		"mcl_nether:netherite_scrap", "mcl_core:gold_ingot", "mcl_core:gold_ingot",
+		"mcl_core:gold_ingot", "mcl_core:gold_ingot", },
 })
 
 minetest.register_craft({
