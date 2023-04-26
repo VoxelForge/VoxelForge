1f207f214f7906156324ab02c19fa9f218c5de0a
cora <coradelamouche@gmx.ch>
2023-04-27 00:02:30 +0200

    Add param2 coloring by "biomecolor" group

--- mods/ITEMS/mcl_core/nodes_base.lua
+++ mods/ITEMS/mcl_core/nodes_base.lua
@@ -384,7 +384,7 @@ minetest.register_node("mcl_core:dirt_with_grass", {
 		handy = 1, shovely = 1, dirt = 2, grass_block = 1, grass_block_no_snow = 1,
 		soil = 1, soil_sapling = 2, soil_sugarcane = 1, cultivatable = 2,
 		spreading_dirt_type = 1, enderman_takable = 1, building_block = 1,
-		compostability = 30,
+		compostability = 30,biomecolor = 1,
 	},
 	drop = "mcl_core:dirt",
 	sounds = mcl_sounds.node_sound_dirt_defaults({
