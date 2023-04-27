cd55abb81685752c9247c88d4a54c26f8651a4bc
cora <coradelamouche@gmx.ch>
2023-04-27 12:28:20 +0200

    Add decaychains API

---
+++ mods/ITEMS/mcl_copper/decaychains.lua
@@ -0,0 +1,152 @@
+mcl_copper.registered_decaychains = {}
+local decay_nodes = {}
+local nodename_chains = {}
+
+function mcl_copper.get_decayed(nodename)
+	local dc = mcl_copper.registered_decaychains[nodename_chains[nodename]]
+	if not dc then return end
+	local ci = table.indexof(dc.nodes,nodename) + 1
+	if ci < 1 then ci = 1 end
+	if ci > #dc.nodes then ci = #dc.nodes end
+	return dc.nodes[ci]
+end
+
+function mcl_copper.get_undecayed(nodename)
+	local dc = mcl_copper.registered_decaychains[nodename_chains[nodename]]
+	if not dc then return end
+	local ci = table.indexof(dc.nodes,nodename) - 1
+	if ci < 1 then ci = 1 end
+	if ci > #dc.nodes then ci = #dc.nodes end
+	return dc.nodes[ci]
+end
+
+local function anti_oxidation_particles(pointed_thing)
+	local pos = pointed_thing.under
+	minetest.add_particlespawner({
+		amount = 8,
+		time = 1,
+		minpos = pos - 1,
+		maxpos = pos + 1,
+		minvel = vector.zero(),
+		maxvel = vector.zero(),
+		minacc = vector.zero(),
+		maxacc = vector.zero(),
+		minexptime = 0.5,
+		maxexptime = 1,
+		minsize = 1,
+		maxsize = 2.5,
+		collisiondetection = false,
+		vertical = false,
+		texture = "mcl_stone_copper_anti_oxidation_particle.png",
+		glow = 5,
+	})
+end
+
+local function unpreserve(pos, node, clicker, itemstack, pointed_thing)
+	local node = minetest.get_node(pointed_thing.under)
+	local unpreserved = node.name:gsub("_preserved","")
+	local def = mcl_copper.registered_decaychains[nodename_chains[unpreserved]]
+	if minetest.registered_nodes[unpreserved] then
+		node.name = unpreserved
+		minetest.swap_node(pointed_thing.under,node)
+		if not minetest.is_creative_enabled(clicker:get_player_name()) then
+			itemstack:add_wear_by_uses(65536)
+		end
+	end
+	return itemstack
+end
+
+local function undecay(pos, node, clicker, itemstack, pointed_thing)
+	local node = minetest.get_node(pointed_thing.under)
+	node.name = mcl_copper.get_undecayed(node.name)
+	minetest.swap_node(pointed_thing.under,node)
+	if not minetest.is_creative_enabled(clicker:get_player_name()) then
+		itemstack:add_wear_by_uses(65536)
+	end
+	return itemstack
+end
+
+function mcl_copper.register_decaychain(name,def)
+	mcl_copper.registered_decaychains[name] = def
+	assert(type(def.nodes) == "table","[mcl_stone] Failed to register decaychain "..tostring(name)..": field nodes is not a table.")
+	for k,v in ipairs(def.nodes) do
+		nodename_chains[v] = name
+		if k <= #def.nodes then
+			table.insert(decay_nodes,v)
+			if k < #def.nodes and def.preserve_group then
+				local od = minetest.registered_nodes[v]
+				assert(type(od) == "table","[mcl_stone] Failed to register decaychain "..tostring(name)..": one of the nodes in the chain does not exist: "..tostring(v))
+				local nd = table.copy(od)
+				nd.description = (def.preserved_description or S("Preserved ") )..nd.description
+				if def.unpreserve_group then
+					nd.on_rightclick  = function(pos, node, clicker, itemstack, pointed_thing)
+						if minetest.get_item_group(itemstack:get_name(),def.unpreserve_group) == 0 then
+							if od.on_rightclick  then return od.on_rightclick(pos, node, clicker, itemstack, pointed_thing) end
+							if minetest.item_place_node(itemstack, clocker, pointed_thing) and not minetest.is_creative_enabled(clicker:get_player_name()) then
+								itemstack:take_item()
+							end
+						elseif minetest.get_item_group(itemstack:get_name(),def.unpreserve_group) > 0 then
+							return unpreserve(pos, node, clicker, itemstack, pointed_thing)
+						end
+						return itemstack
+					end
+				end
+				minetest.register_node(v.."_preserved",nd)
+			end
+			if k > 1 and def.undecay_group then
+				local old_or = minetest.registered_items[v].on_rightclick
+				minetest.override_item(v,{
+					on_rightclick  = function(pos, node, clicker, itemstack, pointed_thing)
+						if minetest.get_item_group(itemstack:get_name(),def.unpreserve_group) == 0 then
+							if old_or  then return old_or(pos, node, clicker, itemstack, pointed_thing) end
+							if minetest.item_place_node(itemstack, clocker, pointed_thing) and not minetest.is_creative_enabled(clicker:get_player_name()) then
+								itemstack:take_item()
+							end
+						elseif minetest.get_item_group(itemstack:get_name(),def.undecay_group) > 0 then
+							return undecay(pos, node, clicker, itemstack, pointed_thing)
+						end
+						return itemstack
+					end
+				})
+			end
+		end
+	end
+end
+
+minetest.register_on_mods_loaded(function()
+	minetest.register_abm({
+		label = "Node Decay",
+		nodenames = decay_nodes,
+		interval = 500,
+		chance = 3,
+		action = function(pos, node)
+			local dc = mcl_copper.get_decayed(node.name)
+			if not dc then return end
+			minetest.swap_node(pos, {name = dc, param2 = node.param2})
+		end,
+	})
+	for k,v in pairs(mcl_copper.registered_decaychains) do
+		if v.preserve_group then
+			for it,def in pairs(minetest.registered_items) do
+				if minetest.get_item_group(it,v.preserve_group) > 0 then
+					local old_op = def.on_place
+					minetest.override_item(it,{
+						on_place =  function(itemstack, placer, pointed_thing)
+							local node = minetest.get_node(pointed_thing.under)
+							if table.indexof(v.nodes,node.name) == -1 then
+								if old_op then return old_op(itemstack, placer, pointed_thing) end
+							elseif table.indexof(v.nodes,node.name) < #v.nodes then
+								node.name = node.name.."_preserved"
+								minetest.swap_node(pointed_thing.under,node)
+								if not minetest.is_creative_enabled(placer:get_player_name()) then
+									itemstack:take_item()
+								end
+							end
+							return itemstack
+						end
+					})
+				end
+			end
+		end
+	end
+end)
--- mods/ITEMS/mcl_copper/init.lua
+++ mods/ITEMS/mcl_copper/init.lua
@@ -1,6 +1,22 @@
+mcl_copper = {}
 local path = minetest.get_modpath("mcl_copper")
 
-dofile(path .. "/functions.lua")
+dofile(path .. "/decaychains.lua")
+--dofile(path .. "/functions.lua")
 dofile(path .. "/nodes.lua")
 dofile(path .. "/items.lua")
 dofile(path .. "/crafting.lua")
+
+mcl_copper.register_decaychain("copper",{
+	preserved_description = "Waxed ",
+	preserve_group = "preserves_copper",
+	unpreserve_group = "axe",
+    --decay_group = "oxidizes_copper",
+	undecay_group = "axe",
+	nodes = { --order is significant
+		"mcl_copper:block",
+		"mcl_copper:block_exposed",
+		"mcl_copper:block_weathered",
+		"mcl_copper:block_oxidized",
+	},
+})
