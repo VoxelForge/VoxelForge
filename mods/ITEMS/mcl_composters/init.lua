VoxeLibre/VoxeLibre

Tweak volume and change reference to missing global variable

mods/ITEMS/mcl_composters/init.lua
@@ -76,7 +76,7 @@ local function composter_add_item(pos, node, player, itemstack, pointed_thing)
 	if chance > 0 then
 		if not is_creative_enabled(player:get_player_name()) then
 			itemstack:take_item()
-			minetest.sound_play({name="default_gravel_dug", pos=above, gain=1}, true)
+			minetest.sound_play({name="default_gravel_dug", pos=pos, gain=1}, true)
 		end
 		-- calculate leveling up chance
 		local rand = math.random(0,100)
@@ -92,7 +92,7 @@ local function composter_add_item(pos, node, player, itemstack, pointed_thing)
 				level = "ready"
 			end
 			swap_node(pos, {name = "mcl_composters:composter_" .. level})
-			minetest.sound_play({name="default_grass_footstep", pos=above, gain=1}, true)
+			minetest.sound_play({name="default_grass_footstep", pos=pos, gain=0.4}, true)
 			-- a full composter becomes ready for harvest after one second
 			-- the block will get updated by the node timer callback set in node reg def
 			if level == 7 then

VoxelForge/VoxelForge

mods/ITEMS/mcl_composters/init.lua
@@ -66,7 +66,7 @@ local function composter_add_item(pos, node, player, itemstack, pointed_thing)
 	if chance > 0 then
 		if not minetest.is_creative_enabled(player:get_player_name()) then
 			itemstack:take_item()
-			minetest.sound_play({name="default_gravel_dug", pos=above, gain=1}, true)
+			minetest.sound_play({name="default_gravel_dug", pos=pos, gain=1}, true)
 		end
 		-- calculate leveling up chance
 		local rand = math.random(0,100)
@@ -82,7 +82,7 @@ local function composter_add_item(pos, node, player, itemstack, pointed_thing)
 				level = "ready"
 			end
 			minetest.swap_node(pos, {name = "mcl_composters:composter_" .. level})
-			minetest.sound_play({name="default_grass_footstep", pos=above, gain=1}, true)
+			minetest.sound_play({name="default_grass_footstep", pos=above, gain=0.4}, true)
 			-- a full composter becomes ready for harvest after one second
 			-- the block will get updated by the node timer callback set in node reg def
 			if level == 7 then