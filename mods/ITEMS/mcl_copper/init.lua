f75c96f0203bef4a5956a064d5a0e1b107191bfe
cora <coradelamouche@gmx.ch>
2023-05-06 12:07:09 +0200

    Remove superfluous comment

--- mods/ITEMS/mcl_copper/init.lua
+++ mods/ITEMS/mcl_copper/init.lua
@@ -10,7 +10,6 @@ mcl_copper.register_decaychain("copper",{
 	preserved_description = "Waxed ",
 	preserve_group = "preserves_copper",
 	unpreserve_group = "axe",
-    --decay_group = "oxidizes_copper",
 	undecay_group = "axe",
 	nodes = { --order is significant
 		"mcl_copper:block",
