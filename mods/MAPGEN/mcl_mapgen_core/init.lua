124d6089008e7a1040ad6ffd91284f940e13d226
cora <coradelamouche@gmx.ch>
2023-05-09 01:19:54 +0200

    Remove v6 mapgen aliases

--- mods/MAPGEN/mcl_mapgen_core/init.lua
+++ mods/MAPGEN/mcl_mapgen_core/init.lua
@@ -14,44 +14,15 @@ local modpath = minetest.get_modpath(modname)
 
 minetest.register_alias("mapgen_air", "air")
 minetest.register_alias("mapgen_stone", "mcl_core:stone")
-minetest.register_alias("mapgen_tree", "mcl_core:tree")
-minetest.register_alias("mapgen_leaves", "mcl_core:leaves")
-minetest.register_alias("mapgen_jungletree", "mcl_core:jungletree")
-minetest.register_alias("mapgen_jungleleaves", "mcl_core:jungleleaves")
-minetest.register_alias("mapgen_pine_tree", "mcl_core:sprucetree")
-minetest.register_alias("mapgen_pine_needles", "mcl_core:spruceleaves")
-
-minetest.register_alias("mapgen_apple", "mcl_core:leaves")
 minetest.register_alias("mapgen_water_source", "mcl_core:water_source")
-minetest.register_alias("mapgen_dirt", "mcl_core:dirt")
-minetest.register_alias("mapgen_dirt_with_grass", "mcl_core:dirt_with_grass")
-minetest.register_alias("mapgen_dirt_with_snow", "mcl_core:dirt_with_grass_snow")
-minetest.register_alias("mapgen_sand", "mcl_core:sand")
-minetest.register_alias("mapgen_gravel", "mcl_core:gravel")
-minetest.register_alias("mapgen_clay", "mcl_core:clay")
 minetest.register_alias("mapgen_lava_source", "air") -- Built-in lava generator is too unpredictable, we generate lava on our own
 minetest.register_alias("mapgen_cobble", "mcl_core:cobble")
-minetest.register_alias("mapgen_mossycobble", "mcl_core:mossycobble")
-minetest.register_alias("mapgen_junglegrass", "mcl_flowers:fern")
-minetest.register_alias("mapgen_stone_with_coal", "mcl_core:stone_with_coal")
-minetest.register_alias("mapgen_stone_with_iron", "mcl_core:stone_with_iron")
-minetest.register_alias("mapgen_desert_sand", "mcl_core:sand")
-minetest.register_alias("mapgen_desert_stone", "mcl_core:sandstone")
-minetest.register_alias("mapgen_sandstone", "mcl_core:sandstone")
+
 if minetest.get_modpath("mclx_core") then
 	minetest.register_alias("mapgen_river_water_source", "mclx_core:river_water_source")
 else
 	minetest.register_alias("mapgen_river_water_source", "mcl_core:water_source")
 end
-minetest.register_alias("mapgen_snow", "mcl_core:snow")
-minetest.register_alias("mapgen_snowblock", "mcl_core:snowblock")
-minetest.register_alias("mapgen_ice", "mcl_core:ice")
-
-minetest.register_alias("mapgen_stair_cobble", "mcl_stairs:stair_cobble")
-minetest.register_alias("mapgen_sandstonebrick", "mcl_core:sandstonesmooth")
-minetest.register_alias("mapgen_stair_sandstonebrick", "mcl_stairs:stair_sandstone")
-minetest.register_alias("mapgen_stair_sandstone_block", "mcl_stairs:stair_sandstone")
-minetest.register_alias("mapgen_stair_desert_stone", "mcl_stairs:stair_sandstone")
 
 dofile(modpath.."/api.lua")
 dofile(modpath.."/ores.lua")
