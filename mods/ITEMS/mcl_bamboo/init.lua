43cd5effc16de98fb76cbe5a60bfa877557c7d18
cora <coradelamouche@gmx.ch>
2023-05-16 22:20:32 +0200

    Add cleaner registration of api nodes

--- mods/ITEMS/mcl_bamboo/init.lua
+++ mods/ITEMS/mcl_bamboo/init.lua
@@ -2,4 +2,5 @@ local modname = minetest.get_current_modname()
 mcl_bamboo = {}
 
 dofile(minetest.get_modpath(modname) .. "/nodes.lua")
+dofile(minetest.get_modpath(modname) .. "/register.lua")
 dofile(minetest.get_modpath(modname) .. "/recipes.lua")
