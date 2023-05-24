532dadc0f93eb401a8d4fc92ea85854188eda1de
cora <coradelamouche@gmx.ch>
2023-05-25 00:11:11 +0200

    Remove commented out p2 timinig logging

--- mods/MAPGEN/mcl_mapgen_core/init.lua
+++ mods/MAPGEN/mcl_mapgen_core/init.lua
@@ -250,7 +250,6 @@ minetest.register_on_mods_loaded(function()
 end)
 
 local function set_param2_nodes(vm, data, data2, emin, emax, area, minp, maxp, blockseed)
-	local t1 = os.clock()
 	local lvm_used = false
 	if minp.y <= mcl_vars.mg_overworld_max and maxp.y >= mcl_vars.mg_overworld_min then
 		for z = minp.z, maxp.z do
@@ -266,7 +265,6 @@ local function set_param2_nodes(vm, data, data2, emin, emax, area, minp, maxp, b
 		end
 		end
 	end
-	--minetest.log(os.clock()-t1)
 	return lvm_used
 end
 
