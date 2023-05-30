91df9ebf9f6dc6bdf50a42fb3a01867d7c3425fb
epCode <eeliperry@gmail.com>
2023-05-29 21:59:35 -0700

    Incorperate sheep eating animation. #3734

--- mods/ENTITIES/mcl_mobs/movement.lua
+++ mods/ENTITIES/mcl_mobs/movement.lua
@@ -456,6 +456,7 @@ end
 -- find and replace what mob is looking for (grass, wheat etc.)
 function mob_class:replace(pos)
 
+
 	if not self.replace_rate
 	or not self.replace_what
 	or self.child == true
@@ -495,6 +496,8 @@ function mob_class:replace(pos)
 		if on_replace_return ~= false then
 
 			if mobs_griefing then
+				self.state = "eat"
+				self:set_animation("eat")
 				minetest.set_node(pos, newnode)
 			end
 
