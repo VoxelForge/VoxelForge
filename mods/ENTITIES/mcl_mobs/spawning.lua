ff4efe2e0573551416871b78b48813d38f38067a
cora <coradelamouche@gmx.ch>
2023-06-01 12:37:30 +0200

    Default to max_light 7 for hostile mobs

--- mods/ENTITIES/mcl_mobs/spawning.lua
+++ mods/ENTITIES/mcl_mobs/spawning.lua
@@ -118,7 +118,7 @@ function mcl_mobs.spawn_setup(def)
 	local biomes           = def.biomes
 	local biomes_except    = def.biomes_except
 	local min_light        = def.min_light or (def.spawn_class == "hostile" and 0 or 7)
-	local max_light        = def.max_light or (minetest.LIGHT_MAX + 1)
+	local max_light        = def.max_light or (def.spawn_class == "hostile" and 7 or minetest.LIGHT_MAX + 1)
 	local chance           = def.chance or 1000
 	local aoc              = def.aoc or aoc_range
 	local min_height       = def.min_height or mcl_vars["mg_"..dimension.."_min"]
