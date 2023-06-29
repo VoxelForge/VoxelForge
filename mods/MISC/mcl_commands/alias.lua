9572d1f10b56a4eddc4e080aa7c24281f377392d
Elias Åström <ryvnf@riseup.net>
2023-06-29 22:12:09 +0200

    Restore builtin commands

--- mods/MISC/mcl_commands/alias.lua
+++ mods/MISC/mcl_commands/alias.lua
@@ -5,26 +5,18 @@ local function register_chatcommand_alias(alias, cmd)
 	minetest.register_chatcommand(alias, def)
 end
 
-local function rename_chatcommand(newname, cmd)
-	local def = minetest.chatcommands[cmd]
-	minetest.register_chatcommand(newname, def)
-	minetest.unregister_chatcommand(cmd)
-end
+register_chatcommand_alias("?", "help")
+register_chatcommand_alias("pardon", "unban")
+register_chatcommand_alias("stop", "shutdown")
+register_chatcommand_alias("tell", "msg")
+register_chatcommand_alias("w", "msg")
+register_chatcommand_alias("tp", "teleport")
+register_chatcommand_alias("clear", "clearinv")
 
-if minetest.settings:get_bool("mcl_builtin_commands_overide", true) then
-	register_chatcommand_alias("?", "help")
-	register_chatcommand_alias("pardon", "unban")
-	rename_chatcommand("stop", "shutdown")
-	register_chatcommand_alias("tell", "msg")
-	register_chatcommand_alias("w", "msg")
-	register_chatcommand_alias("tp", "teleport")
-	rename_chatcommand("clear", "clearinv")
-
-	minetest.register_chatcommand("banlist", {
-		description = S("List bans"),
-		privs = minetest.chatcommands["ban"].privs,
-		func = function(name)
-			return true, S("Ban list: @1", minetest.get_ban_list())
-		end,
-	})
-end
+minetest.register_chatcommand("banlist", {
+	description = S("List bans"),
+	privs = minetest.chatcommands["ban"].privs,
+	func = function(name)
+		return true, S("Ban list: @1", minetest.get_ban_list())
+	end,
+})
