777f48fa4b6e1645196fa147cddeb80b71a55dfc
cora <coradelamouche@gmx.ch>
2023-07-21 11:50:00 +0200

    Restructure functions

--- mods/ITEMS/mcl_signs/init.lua
+++ mods/ITEMS/mcl_signs/init.lua
@@ -13,6 +13,74 @@ local DEFAULT_COLOR = "#000000"
 local S = minetest.get_translator(minetest.get_current_modname())
 local F = minetest.formspec_escape
 
+local sign_tpl = {
+	paramtype = "light",
+	description = S("Sign"),
+	_tt_help = S("Can be written"),
+	_doc_items_longdesc = S("Signs can be written and come in two variants: Wall sign and sign on a sign post. Signs can be placed on the top and the sides of other blocks, but not below them."),
+	_doc_items_usagehelp = S("After placing the sign, you can write something on it. You have 4 lines of text with up to 15 characters for each line; anything beyond these limits is lost. Not all characters are supported. The text can not be changed once it has been written; you have to break and place the sign again. Can be colored and made to glow."),
+	use_texture_alpha = minetest.features.use_texture_alpha_string_modes and "opaque" or false,
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	paramtype2 = "degrotate",
+	drawtype = "mesh",
+	mesh = "mcl_signs_sign.obj",
+	inventory_image = "default_sign.png",
+	wield_image = "default_sign.png",
+	selection_box = { type = "fixed", fixed = { -0.2, -0.5, -0.2, 0.2, 0.5, 0.2 } },
+	tiles = { "mcl_signs_sign.png" },
+	groups = { axey = 1, handy = 2, sign = 1, not_in_creative_inventory = 1 },
+	drop = "mcl_signs:sign",
+	stack_max = 16,
+	sounds = mcl_sounds.node_sound_wood_defaults(),
+	node_placement_prediction = "",
+}
+
+--Signs data / meta
+local function normalize_rotation(rot) return math.floor(0.5 + rot / 15) * 15 end
+
+local function get_signdata(pos)
+	local node = minetest.get_node(pos)
+	local def = minetest.registered_nodes[node.name]
+	local meta = minetest.get_meta(pos)
+	local text = meta:get_string("text")
+	local color = meta:get_string("color")
+	local yaw, spos
+	local typ = "standing"
+	if def.paramtype2  == "wallmounted" then
+		typ = "wall"
+		local dir = minetest.wallmounted_to_dir(node.param2)
+		spos = vector.add(vector.offset(pos,0,-0.25,0),dir * 0.41 )
+		yaw = minetest.dir_to_yaw(dir)
+	elseif def.paramtype2 == "facedir" then
+		typ = "hanging"
+		local dir = minetest.facedir_to_dir(node.param2)
+		spos = vector.add(vector.offset(pos,0,-0.25,0),dir * -0.05 )
+		yaw = minetest.dir_to_yaw(dir)
+	else
+		yaw = math.rad(((node.param2 * 1.5 ) + 1 ) % 360)
+		local dir = minetest.yaw_to_dir(yaw)
+		spos = vector.add(vector.offset(pos,0,0.08,0),dir * -0.05)
+	end
+	if color == "" then color = DEFAULT_COLOR end
+	return {
+		text = text,
+		color = color,
+		yaw = yaw,
+		node = node,
+		typ = typ,
+		text_pos = spos,
+	}
+end
+
+local function set_signmeta(pos,def)
+	local meta = minetest.get_meta(pos)
+	if def.text then meta:set_string("text",def.text) end
+	if def.color then meta:set_string("color",def.color) end
+end
+
+-- Text/texture
 --[[ File format of characters.txt:
 It's an UTF-8 encoded text file that contains metadata for all supported characters. It contains a sequence of info
 	blocks, one for each character. Each info block is made out of 3 lines:
@@ -127,73 +195,6 @@ function mcl_signs.generate_texture(data)
 	return texture
 end
 
-local function normalize_rotation(rot) return math.floor(0.5 + rot / 15) * 15 end
-
-local function get_signdata(pos)
-	local node = minetest.get_node(pos)
-	local def = minetest.registered_nodes[node.name]
-	local meta = minetest.get_meta(pos)
-	local text = meta:get_string("text")
-	local color = meta:get_string("color")
-	local yaw, spos
-	local typ = "standing"
-	if def.paramtype2  == "wallmounted" then
-		typ = "wall"
-		local dir = minetest.wallmounted_to_dir(node.param2)
-		spos = vector.add(vector.offset(pos,0,-0.25,0),dir * 0.41 )
-		yaw = minetest.dir_to_yaw(dir)
-	elseif def.paramtype2 == "facedir" then
-		typ = "hanging"
-		local dir = minetest.facedir_to_dir(node.param2)
-		spos = vector.add(vector.offset(pos,0,-0.25,0),dir * -0.05 )
-		yaw = minetest.dir_to_yaw(dir)
-	else
-		yaw = math.rad(((node.param2 * 1.5 ) + 1 ) % 360)
-		local dir = minetest.yaw_to_dir(yaw)
-		spos = vector.add(vector.offset(pos,0,0.08,0),dir * -0.05)
-	end
-	if color == "" then color = DEFAULT_COLOR end
-	return {
-		text = text,
-		color = color,
-		yaw = yaw,
-		node = node,
-		typ = typ,
-		text_pos = spos,
-	}
-end
-
-local function set_signmeta(pos,def)
-	local meta = minetest.get_meta(pos)
-	if def.text then meta:set_string("text",def.text) end
-	if def.color then meta:set_string("color",def.color) end
-end
-
-
-local sign_tpl = {
-	paramtype = "light",
-	description = S("Sign"),
-	_tt_help = S("Can be written"),
-	_doc_items_longdesc = S("Signs can be written and come in two variants: Wall sign and sign on a sign post. Signs can be placed on the top and the sides of other blocks, but not below them."),
-	_doc_items_usagehelp = S("After placing the sign, you can write something on it. You have 4 lines of text with up to 15 characters for each line; anything beyond these limits is lost. Not all characters are supported. The text can not be changed once it has been written; you have to break and place the sign again. Can be colored and made to glow."),
-	use_texture_alpha = minetest.features.use_texture_alpha_string_modes and "opaque" or false,
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	paramtype2 = "degrotate",
-	drawtype = "mesh",
-	mesh = "mcl_signs_sign.obj",
-	inventory_image = "default_sign.png",
-	wield_image = "default_sign.png",
-	selection_box = { type = "fixed", fixed = { -0.2, -0.5, -0.2, 0.2, 0.5, 0.2 } },
-	tiles = { "mcl_signs_sign.png" },
-	groups = { axey = 1, handy = 2, sign = 1, not_in_creative_inventory = 1 },
-	drop = "mcl_signs:sign",
-	stack_max = 16,
-	sounds = mcl_sounds.node_sound_wood_defaults(),
-	node_placement_prediction = "",
-}
-
 function sign_tpl.on_place(itemstack, placer, pointed_thing)
 	if pointed_thing.type ~= "node" then
 		return itemstack
@@ -261,21 +262,34 @@ table.update(sign_hanging,{
 	groups = { axey = 1, handy = 2, sign = 1, not_in_creative_inventory = 1 },
 })
 
-minetest.register_node("mcl_signs:sign_standing",sign_tpl)
-minetest.register_node("mcl_signs:sign_hanging",sign_hanging)
-minetest.register_node("mcl_signs:sign",sign_wall)
-
-function mcl_signs.register_sign(name,color,def)
-	local tiles = { "mcl_signs_sign_greyscale.png^[multiply:"..color }
-	local inventory_image = "default_sign.png^[multiply:"..color
-	def = def or {}
-	minetest.register_node(":mcl_signs:"..name.."_sign_standing",table.update({},sign_tpl,{tiles = tiles},def))
-	minetest.register_node(":mcl_signs:"..name.."_sign_hanging",table.update({},sign_hanging,{tiles = tiles},def))
-	minetest.register_node(":mcl_signs:"..name.."_sign",table.update({},sign_wall,{tiles = tiles},def))
+--Formspec
+function mcl_signs.show_formspec(player, pos)
+	minetest.show_formspec(
+			player:get_player_name(),
+			"mcl_signs:set_text_" .. pos.x .. "_" .. pos.y .. "_" .. pos.z,
+			"size[6,3]textarea[0.25,0.25;6,1.5;text;" ..
+			F(S("Enter sign text:")) .. ";]label[0,1.5;" ..
+			F(S("Maximum line length: 15")) .. "\n" ..
+			F(S("Maximum lines: 4")) ..
+			"]button_exit[0,2.5;6,1;submit;" .. F(S("Done")) .. "]"
+	)
 end
 
-mcl_signs.register_sign("bluetest","#0000FF")
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname:find("mcl_signs:set_text_") == 1 then
+		local x, y, z = formname:match("mcl_signs:set_text_(.-)_(.-)_(.*)")
+		local pos = { x = tonumber(x), y = tonumber(y), z = tonumber(z) }
+		if not pos or not pos.x or not pos.y or not pos.z then
+			return
+		end
+		set_signmeta(pos,{
+			text = fields.text,
+		})
+		mcl_signs.update_sign(pos)
+	end
+end)
 
+--Text entity handling
 function mcl_signs.get_text_entity (pos, force_remove)
 	local objects = minetest.get_objects_inside_radius(pos, 0.5)
 	local text_entity
@@ -313,17 +327,15 @@ function mcl_signs.update_sign(pos)
 	return true
 end
 
-function mcl_signs.show_formspec(player, pos)
-	minetest.show_formspec(
-			player:get_player_name(),
-			"mcl_signs:set_text_" .. pos.x .. "_" .. pos.y .. "_" .. pos.z,
-			"size[6,3]textarea[0.25,0.25;6,1.5;text;" ..
-			F(S("Enter sign text:")) .. ";]label[0,1.5;" ..
-			F(S("Maximum line length: 15")) .. "\n" ..
-			F(S("Maximum lines: 4")) ..
-			"]button_exit[0,2.5;6,1;submit;" .. F(S("Done")) .. "]"
-	)
-end
+minetest.register_lbm({
+	nodenames = {"group:sign"},
+	name = "mcl_signs:restore_entities",
+	label = "Restore sign text",
+	run_at_every_load = true,
+	action = function(pos, node)
+		mcl_signs.update_sign(pos)
+	end
+})
 
 minetest.register_entity("mcl_signs:text", {
 	pointable = false,
@@ -344,26 +356,21 @@ minetest.register_entity("mcl_signs:text", {
 	end,
 })
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname:find("mcl_signs:set_text_") == 1 then
-		local x, y, z = formname:match("mcl_signs:set_text_(.-)_(.-)_(.*)")
-		local pos = { x = tonumber(x), y = tonumber(y), z = tonumber(z) }
-		if not pos or not pos.x or not pos.y or not pos.z then
-			return
-		end
-		set_signmeta(pos,{
-			text = fields.text,
-		})
-		mcl_signs.update_sign(pos)
-	end
-end)
+minetest.register_node("mcl_signs:sign_standing",sign_tpl)
+minetest.register_node("mcl_signs:sign_hanging",sign_hanging)
+minetest.register_node("mcl_signs:sign",sign_wall)
 
-minetest.register_lbm({
-	nodenames = {"group:sign"},
-	name = "mcl_signs:restore_entities",
-	label = "Restore sign text",
-	run_at_every_load = true,
-	action = function(pos, node)
-		mcl_signs.update_sign(pos)
-	end
-})
+function mcl_signs.register_sign(name,color,def)
+	local textures = {
+		tiles = { "mcl_signs_sign_greyscale.png^[multiply:"..color },
+		inventory_image = "default_sign.png^[multiply:"..color,
+		wield_image = "default_sign.png^[multiply:"..color,
+	}
+	minetest.register_node(":mcl_signs:"..name.."_sign_standing",table.update({},sign_tpl, textures ,def or {}))
+	minetest.register_node(":mcl_signs:"..name.."_sign_hanging",table.update({},sign_hanging,textures,def or {}))
+	minetest.register_node(":mcl_signs:"..name.."_sign",table.update({},sign_wall,textures,def or {}))
+end
+
+mcl_signs.register_sign("redtest","#FF0000")
+mcl_signs.register_sign("bluetest","#0000FF")
+mcl_signs.register_sign("greentest","#00FF00")
