35c72cd16d911495bb5d461eb9c95feb8dd7b8df
cora <coradelamouche@gmx.ch>
2023-07-28 02:16:54 +0200

    Restructure and clarify dye alias file with comments

--- mods/ITEMS/mcl_dyes/alias.lua
+++ mods/ITEMS/mcl_dyes/alias.lua
@@ -1,3 +1,4 @@
+-- Legacy (mcl2) "mcl_dye" itemstrings
 minetest.register_alias("mcl_dye:grey","mcl_dyes:silver")
 minetest.register_alias("mcl_dye:dark_grey","mcl_dyes:grey")
 minetest.register_alias("mcl_dye:violet","mcl_dyes:purple")
@@ -11,16 +12,16 @@ minetest.register_alias("mcl_dye:red","mcl_dyes:red")
 minetest.register_alias("mcl_dye:magenta","mcl_dyes:magenta")
 minetest.register_alias("mcl_dye:pink","mcl_dyes:pink")
 
--- Old messy colornames
-minetest.register_alias("mcl_dyes:dark_green","mcl_dyes:green")
-minetest.register_alias("mcl_dyes:dark_grey","mcl_dyes:grey")
-minetest.register_alias("mcl_dyes:grey","mcl_dyes:silver")
-minetest.register_alias("mcl_dyes:violet","mcl_dyes:purple")
-minetest.register_alias("mcl_dyes:lightblue","mcl_dyes:light_blue")
-
 -- these 4 used to double as other items, aliases to the old items
 -- are provided so people do not loose their hard earned lapis etc.
 minetest.register_alias("mcl_dye:white","mcl_bone_meal:bone_meal")
 minetest.register_alias("mcl_dye:black","mcl_mobitems:ink_sac")
 minetest.register_alias("mcl_dye:blue","mcl_core:lapis")
 minetest.register_alias("mcl_dye:brown","mcl_cocoas:cocoa_beans")
+
+-- Old messy colornames - not following the color naming scheme most of the codebase uses
+minetest.register_alias("mcl_dyes:dark_green","mcl_dyes:green")
+minetest.register_alias("mcl_dyes:dark_grey","mcl_dyes:grey")
+minetest.register_alias("mcl_dyes:grey","mcl_dyes:silver")
+minetest.register_alias("mcl_dyes:violet","mcl_dyes:purple")
+minetest.register_alias("mcl_dyes:lightblue","mcl_dyes:light_blue")
