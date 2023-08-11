694eb02850a983b427641b6767e8bd15557f02b0
cora <coradelamouche@gmx.ch>
2023-08-11 15:55:54 +0200

    Remove TODO about composter sounds
    added in 093aa4ce33551357c076f7bec933f5df085f7ca9

--- mods/ITEMS/mcl_composters/init.lua
+++ mods/ITEMS/mcl_composters/init.lua
@@ -140,7 +140,6 @@ local function composter_harvest(pos, node, player, itemstack, pointed_thing)
 	-- reset ready type composter to empty type
 	minetest.swap_node(pos, {name="mcl_composters:composter"})
 	minetest.add_item(pos, "mcl_bone_meal:bone_meal")
-	-- TODO play some sounds
 	return itemstack
 end
 
