e50a875a2d493ae14c5302e21f8ce6549a9bcf16
cora <coradelamouche@gmx.ch>
2023-08-24 07:04:58 +0200

    Add basic offhand wieldview

--- mods/PLAYER/mcl_wieldview/init.lua
+++ mods/PLAYER/mcl_wieldview/init.lua
@@ -14,9 +14,12 @@ minetest.register_entity("mcl_wieldview:wieldview", {
 })
 
 local wieldview_luaentites = {}
+local offhand_luaentites = {}
 
 local function update_wieldview_entity(player)
 	local luaentity = wieldview_luaentites[player]
+	local offhand_ent = offhand_luaentites[player]
+
 	if luaentity and luaentity.object:get_yaw() then
 		local item = player:get_wielded_item():get_name()
 
@@ -44,16 +47,46 @@ local function update_wieldview_entity(player)
 		local obj_ref = minetest.add_entity(player:get_pos(), "mcl_wieldview:wieldview")
 		if not obj_ref then return end
 		obj_ref:set_attach(player, "Wield_Item")
+		--offhand_ref:set_attach(player, "Hand_Left", vector.new(0, 1, 0), vector.new(90, 45, 90))
 		--obj_ref:set_attach(player, "Hand_Right", vector.new(0, 1, 0), vector.new(90, 45, 90))
 		wieldview_luaentites[player] = obj_ref:get_luaentity()
+	end
+
+	if offhand_ent and offhand_ent.object:get_yaw() then
+		local item = mcl_offhand.get_offhand(player):get_name()
+
+		if item == offhand_ent._item then return end
+
+		offhand_ent._item = item
+
+		local def = mcl_offhand.get_offhand(player):get_definition()
+		if def and def._mcl_wieldview_item then
+			item = def._mcl_wieldview_item
+		end
+
+		local item_def = minetest.registered_items[item]
+		offhand_ent.object:set_properties({
+			glow = item_def and item_def.light_source or 0,
+			wield_item = item,
+			is_visible = item ~= ""
+		})
+	else
+		local offhand_ref = minetest.add_entity(player:get_pos(), "mcl_wieldview:wieldview")
+		if not offhand_ref then return end
+		--obj_ref:set_attach(player, "Wield_Item")
+		offhand_ref:set_attach(player, "Arm_Left", vector.new(-0.4, 4.5, 2), vector.new(120, 0, 0))
+		--obj_ref:set_attach(player, "Hand_Right", vector.new(0, 1, 0), vector.new(90, 45, 90))
+		offhand_luaentites[player] = offhand_ref:get_luaentity()
 	end
 end
 
 minetest.register_on_leaveplayer(function(player)
 	if wieldview_luaentites[player] then
 		wieldview_luaentites[player].object:remove()
+		offhand_luaentites[player].object:remove()
 	end
 	wieldview_luaentites[player] = nil
+	offhand_luaentites[player] = nil
 end)
 
 minetest.register_globalstep(function(dtime)
