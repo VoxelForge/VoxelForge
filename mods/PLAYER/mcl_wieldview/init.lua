9c800d279c6fbd5c20a94b261901cc6e401109df
cora <coradelamouche@gmx.ch>
2023-08-24 17:30:41 +0200

    Add offhand hud

--- mods/PLAYER/mcl_wieldview/init.lua
+++ mods/PLAYER/mcl_wieldview/init.lua
@@ -18,6 +18,38 @@ local wieldview_luaentites = {
 	Arm_Left = {},
 }
 
+local offhand_huds = {}
+
+local function offhand_hud(player, texture)
+	return player:hud_add({
+		hud_elem_type = "image",
+		position = {x = 0.1, y = 0.90},
+		scale = {x = 25, y = 25},
+		z_index = -200,
+		text = texture or "blank.png",
+	})
+end
+
+local function update_offhand_hud(player, texture)
+	local hud = offhand_huds[player]
+	local stack = mcl_offhand.get_offhand(player)
+	local empty = stack:is_empty()
+	local shield = minetest.get_item_group(stack:get_name(), "shield") > 0 --shields implement their own hud
+
+	if hud and ( empty or shield ) then
+		player:hud_change(hud, "text", "blank.png")
+		player:hud_remove(hud)
+		offhand_huds[player] = nil
+	elseif empty or shield then
+		return
+	elseif texture then
+		if not hud then
+			offhand_huds[player] = offhand_hud(player, texture)
+		end
+		player:hud_change(offhand_huds[player], "text", texture)
+	end
+end
+
 local function remove_wieldview(player)
 	for bone,_ in pairs(wieldview_luaentites) do
 		if wieldview_luaentites[bone][player] then
@@ -34,7 +66,7 @@ local function update_wieldview_entity(player, bone, position, rotation, get_ite
 		local item = get_item(player):get_name()
 
 		if item == luaentity._item then return end
-
+		if minetest.get_item_group(item, "shield") > 0  then return end
 		luaentity._item = item
 
 		local def = get_item(player):get_definition()
@@ -43,6 +75,9 @@ local function update_wieldview_entity(player, bone, position, rotation, get_ite
 		end
 
 		local item_def = minetest.registered_items[item]
+		if bone == "Arm_Left" then
+			update_offhand_hud(player, item_def and item_def.wield_image or "")
+		end
 		luaentity.object:set_properties({
 			glow = item_def and item_def.light_source or 0,
 			wield_item = item,
