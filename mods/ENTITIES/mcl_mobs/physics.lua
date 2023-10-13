96df4361bec86fd191360e9352eb1af97bc508bb
cora <coradelamouche@gmx.ch>
2023-10-13 03:46:35 +0200

    Make mobs only throw xp when killed by a (non-creative) player

--- mods/ENTITIES/mcl_mobs/physics.lua
+++ mods/ENTITIES/mcl_mobs/physics.lua
@@ -456,7 +456,7 @@ function mob_class:check_for_death(cause, cmi_cause)
 	self:mob_sound("death")
 
 	local function death_handle(self)
-		-- dropped cooked item if mob died in fire or lava
+		local killed_by_player = false
 		if cause == "lava" or cause == "fire" then
 			self:item_drop(true, 0)
 		else
@@ -465,20 +465,23 @@ function mob_class:check_for_death(cause, cmi_cause)
 				local puncher = cmi_cause.puncher
 				if puncher then
 					wielditem = puncher:get_wielded_item()
+					if puncher:is_player() then
+						killed_by_player  = true
+					end
 				end
 			end
 			local cooked = mcl_burning.is_burning(self.object) or mcl_enchanting.has_enchantment(wielditem, "fire_aspect")
 			local looting = mcl_enchanting.get_enchantment(wielditem, "looting")
 			self:item_drop(cooked, looting)
+			if killed_by_player then
+				if ((not self.child) or self.type ~= "animal") and (minetest.get_us_time() - self.xp_timestamp <= math.huge) then
+					local pos = self.object:get_pos()
+					local xp_amount = math.random(self.xp_min, self.xp_max)
 
-			if ((not self.child) or self.type ~= "animal") and (minetest.get_us_time() - self.xp_timestamp <= math.huge) then
-				local pos = self.object:get_pos()
-				local xp_amount = math.random(self.xp_min, self.xp_max)
-
-				if not mcl_sculk.handle_death(pos, xp_amount) then
-					--minetest.log("Xp not thrown")
-					if minetest.is_creative_enabled("") ~= true then
-						mcl_experience.throw_xp(pos, xp_amount)
+					if not mcl_sculk.handle_death(pos, xp_amount) then
+						if not minetest.is_creative_enabled(cmi_cause.puncher:get_player_name()) then
+							mcl_experience.throw_xp(pos, xp_amount)
+						end
 					end
 				end
 			end
