541d9a08a4e9d4e0ba5f979e3dda113059bda89b
cora <coradelamouche@gmx.ch>
2023-10-14 23:17:18 +0200

    Make armor trims item tooltips nicer

--- mods/ITEMS/mcl_armor/trims.lua
+++ mods/ITEMS/mcl_armor/trims.lua
@@ -2,9 +2,17 @@ local modname			   = minetest.get_current_modname()
 local mod_registername	  = modname .. ":"
 local S					 = minetest.get_translator(modname)
 
+local function readable_name(str)
+	str = str:gsub("_", " ")
+    return (str:gsub("^%l", string.upper))
+end
+
 for _, template_name in pairs(mcl_armor.trims.overlays) do
 	minetest.register_craftitem(mod_registername .. template_name, {
-		description	  = S("Smithing Template '@1'", template_name),
+		description	  = S("Smithing Template"),
+		_tt_help = S("@1 Armor Trim",readable_name(template_name)).."\n\n"..
+		minetest.colorize(mcl_colors.GRAY, S("Applies to:")).."\n"..minetest.colorize(mcl_colors.BLUE, " "..S("Armor")).."\n"..
+		minetest.colorize(mcl_colors.GRAY, S("Ingredients:")).."\n"..minetest.colorize(mcl_colors.BLUE, " "..S("Ingot & Crystals")),
 		inventory_image  = template_name .. "_armor_trim_smithing_template.png",
 		groups = { smithing_template  = 1 },
 	})
