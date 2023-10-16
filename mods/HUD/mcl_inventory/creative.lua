465f89e28938b7d54fe8a52edcc9ee3c635e775e
seventeenthShulker <c545d588-7ff2-49b9-b537-0b3f769083ad@anonaddy.me>
2023-10-16 22:07:52 +0200

    Fix random crash when piston breaks node with no player

--- mods/HUD/mcl_inventory/creative.lua
+++ mods/HUD/mcl_inventory/creative.lua
@@ -725,12 +725,14 @@ function minetest.handle_node_drops(pos, drops, digger)
 			for _, item in ipairs(drops) do
 				minetest.add_item(pos, item)
 			end
-		end
-		local inv = digger:get_inventory()
-		if inv then
-			for _, item in ipairs(drops) do
-				if not inv:contains_item("main", item, true) then
-					inv:add_item("main", item)
+		else
+			-- If there is a player
+			local inv = digger:get_inventory()
+			if inv then
+				for _, item in ipairs(drops) do
+					if not inv:contains_item("main", item, true) then
+						inv:add_item("main", item)
+					end
 				end
 			end
 		end
