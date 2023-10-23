88e4564e68d4a5675de0d99f3c7b9d83018c3b2d
cora <coradelamouche@gmx.ch>
2023-10-23 04:15:56 +0200

    Make cherry trees with beehives less common

--- mods/MAPGEN/mcl_biomes/init.lua
+++ mods/MAPGEN/mcl_biomes/init.lua
@@ -4280,7 +4280,7 @@ local function register_decorations()
 			sidelen = 80,
 			noise_params = {
 				offset = 0.007,
-				scale = 0.01,
+				scale = 0.08,
 				spread = {x = 250, y = 250, z = 250},
 				seed = 13+i,
 				octaves = 3,
@@ -4298,12 +4298,12 @@ local function register_decorations()
 			place_on = {"mcl_core:dirt_with_grass"},
 			sidelen = 80,
 			noise_params = {
-				offset = 0.007,
-				scale = 0.006,
+				offset = 0.0005,
+				scale = 0.0001,
 				spread = {x = 250, y = 250, z = 250},
 				seed = 32+i,
 				octaves = 3,
-				persist = 0.6
+				persist = 0.01
 			},
 			biomes = {"CherryGrove"},
 			y_min = 1,
