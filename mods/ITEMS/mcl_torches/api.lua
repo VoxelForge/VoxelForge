90e3b24846994ab0436b479697dc5df961dd657e
cora <coradelamouche@gmx.ch>
2023-11-20 01:32:20 +0100

    Fix potential crash when rightclicking nodes with torch
    i.e. pointed thing was missing from the on_rightclick call, this uses
    the existing mcl_util function for it

--- mods/ITEMS/mcl_torches/api.lua
+++ mods/ITEMS/mcl_torches/api.lua
@@ -194,9 +194,8 @@ function mcl_torches.register_torch(def)
 
 			-- Call on_rightclick if the pointed node defines it
 			if placer and not placer:get_player_control().sneak then
-				if minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].on_rightclick then
-					return minetest.registered_nodes[node.name].on_rightclick(under, node, placer, itemstack) or itemstack
-				end
+				local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
+				if rc then return rc end
 			end
 
 			local above = pointed_thing.above
