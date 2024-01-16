461eb5688288c2b412858ec5371dcc2ea71ef854
Elias Åström <ryvnf@riseup.net>
2024-01-16 21:19:13 +0100

    Remove outdated comments in mcl_portals

--- mods/ITEMS/mcl_portals/init.lua
+++ mods/ITEMS/mcl_portals/init.lua
@@ -6,13 +6,6 @@ mcl_portals = {
 
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
--- Nether portal:
--- Obsidian frame, activated by flint and steel
 dofile(modpath.."/portal_nether.lua")
-
--- End portal (W.I.P):
--- Red nether brick block frame, activated by an eye of ender
 dofile(modpath.."/portal_end.lua")
-
 dofile(modpath.."/portal_gateway.lua")
-
