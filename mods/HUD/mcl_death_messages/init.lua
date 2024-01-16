f42235f56ace6a3e9e2a9db7815080763ccea0d2
Elias Åström <ryvnf@riseup.net>
2024-01-16 21:29:05 +0100

    Migrate mcl_death_messages to official script

--- mods/HUD/mcl_death_messages/init.lua
+++ mods/HUD/mcl_death_messages/init.lua
@@ -1,48 +1,48 @@
 local S = minetest.get_translator(minetest.get_current_modname())
-local W = mcl_curry(S)
+local NS = function(s) return s end
 
 mcl_death_messages = {
 	assist = {},
 	messages = {
 		in_fire = {
-			plain = W("@1 went up in flames"),
-			assist = W("@1 walked into fire whilst fighting @2"),
+			plain = NS("@1 went up in flames"),
+			assist = NS("@1 walked into fire whilst fighting @2"),
 		},
 		lightning_bolt = {
-			plain = W("@1 was struck by lightning"),
-			assist = W("@1 was struck by lightning whilst fighting @2"),
+			plain = NS("@1 was struck by lightning"),
+			assist = NS("@1 was struck by lightning whilst fighting @2"),
 		},
 		on_fire = {
-			plain = W("@1 burned to death"),
-			assist = W("@1 was burnt to a crisp whilst fighting @2"),
+			plain = NS("@1 burned to death"),
+			assist = NS("@1 was burnt to a crisp whilst fighting @2"),
 		},
 		lava = {
-			plain = W("@1 tried to swim in lava"),
-			assist = W("@1 tried to swim in lava to escape @2"),
+			plain = NS("@1 tried to swim in lava"),
+			assist = NS("@1 tried to swim in lava to escape @2"),
 		},
 		hot_floor = {
-			plain = W("@1 discovered the floor was lava"),
-			assist = W("@1 walked into danger zone due to @2"),
+			plain = NS("@1 discovered the floor was lava"),
+			assist = NS("@1 walked into danger zone due to @2"),
 		},
 		in_wall = {
-			plain = W("@1 suffocated in a wall"),
-			assist = W("@1 suffocated in a wall whilst fighting @2"),
+			plain = NS("@1 suffocated in a wall"),
+			assist = NS("@1 suffocated in a wall whilst fighting @2"),
 		},
 		drown = {
-			plain = W("@1 drowned"),
-			assist = W("@1 drowned whilst trying to escape @2"),
+			plain = NS("@1 drowned"),
+			assist = NS("@1 drowned whilst trying to escape @2"),
 		},
 		starve = {
-			plain = W("@1 starved to death"),
-			assist = W("@1 starved to death whilst fighting @2"),
+			plain = NS("@1 starved to death"),
+			assist = NS("@1 starved to death whilst fighting @2"),
 		},
 		cactus = {
-			plain = W("@1 was pricked to death"),
-			assist = W("@1 walked into a cactus whilst trying to escape @2"),
+			plain = NS("@1 was pricked to death"),
+			assist = NS("@1 walked into a cactus whilst trying to escape @2"),
 		},
 		fall = {
-			plain = W("@1 hit the ground too hard"),
-			assist = W("@1 hit the ground too hard whilst trying to escape @2"),
+			plain = NS("@1 hit the ground too hard"),
+			assist = NS("@1 hit the ground too hard whilst trying to escape @2"),
 			-- "@1 fell from a high place" -- for fall distance > 5 blocks
 			-- "@1 fell while climbing"
 			-- "@1 fell off some twisting vines"
@@ -52,81 +52,80 @@ mcl_death_messages = {
 			-- "@1 fell off a ladder"
 		},
 		fly_into_wall = {
-			plain = W("@1 experienced kinetic energy"),
-			assist = W("@1 experienced kinetic energy whilst trying to escape @2"),
+			plain = NS("@1 experienced kinetic energy"),
+			assist = NS("@1 experienced kinetic energy whilst trying to escape @2"),
 		},
 		out_of_world = {
-			plain = W("@1 fell out of the world"),
-			assist = W("@1 didn't want to live in the same world as @2"),
+			plain = NS("@1 fell out of the world"),
+			assist = NS("@1 didn't want to live in the same world as @2"),
 		},
 		generic = {
-			plain = W("@1 died"),
-			assist = W("@1 died because of @2"),
+			plain = NS("@1 died"),
+			assist = NS("@1 died because of @2"),
 		},
 		magic = {
-			plain = W("@1 was killed by magic"),
-			assist = W("@1 was killed by magic whilst trying to escape @2"),
-			killer = W("@1 was killed by @2 using magic"),
-			item = W("@1 was killed by @2 using @3"),
+			plain = NS("@1 was killed by magic"),
+			assist = NS("@1 was killed by magic whilst trying to escape @2"),
+			killer = NS("@1 was killed by @2 using magic"),
+			item = NS("@1 was killed by @2 using @3"),
 		},
 		dragon_breath = {
-			plain = W("@1 was roasted in dragon breath"),
-			killer = W("@1 was roasted in dragon breath by @2"),
+			plain = NS("@1 was roasted in dragon breath"),
+			killer = NS("@1 was roasted in dragon breath by @2"),
 		},
 		wither = {
-			plain = W("@1 withered away"),
-			escape = W("@1 withered away whilst fighting @2"),
+			plain = NS("@1 withered away"),
+			escape = NS("@1 withered away whilst fighting @2"),
 		},
 		wither_skull = {
-			plain = W("@1 was killed by magic"),
-			killer = W("@1 was shot by a skull from @2"),
+			plain = NS("@1 was killed by magic"),
+			killer = NS("@1 was shot by a skull from @2"),
 		},
 		anvil = {
-			plain = W("@1 was squashed by a falling anvil"),
-			escape = W("@1 was squashed by a falling anvil whilst fighting @2"),
+			plain = NS("@1 was squashed by a falling anvil"),
+			escape = NS("@1 was squashed by a falling anvil whilst fighting @2"),
 		},
 		falling_node = {
-			plain = W("@1 was squashed by a falling block"),
-			assist = W("@1 was squashed by a falling block whilst fighting @2"),
+			plain = NS("@1 was squashed by a falling block"),
+			assist = NS("@1 was squashed by a falling block whilst fighting @2"),
 		},
 		mob = {
-			killer = W("@1 was slain by @2"),
-			item = W("@1 was slain by @2 using @3"),
+			killer = NS("@1 was slain by @2"),
+			item = NS("@1 was slain by @2 using @3"),
 		},
 		player = {
-			killer = W("@1 was slain by @2"),
-			item = W("@1 was slain by @2 using @3")
+			killer = NS("@1 was slain by @2"),
+			item = NS("@1 was slain by @2 using @3")
 		},
 		arrow = {
-			killer = W("@1 was shot by @2"),
-			item = W("@1 was shot by @2 using @3"),
+			killer = NS("@1 was shot by @2"),
+			item = NS("@1 was shot by @2 using @3"),
 		},
 		fireball = {
-			killer = W("@1 was fireballed by @2"),
-			item = W("@1 was fireballed by @2 using @3"),
+			killer = NS("@1 was fireballed by @2"),
+			item = NS("@1 was fireballed by @2 using @3"),
 		},
 		thorns = {
-			killer = W("@1 was killed trying to hurt @2"),
-			item = W("@1 tried to hurt @2 and died by @3"),
+			killer = NS("@1 was killed trying to hurt @2"),
+			item = NS("@1 tried to hurt @2 and died by @3"),
 		},
 		explosion = {
-			_translator = S,
-			plain = W("@1 blew up"),
-			killer = W("@1 was blown up by @2"),
-			item = W("@1 was blown up by @2 using @3"),
+			plain = NS("@1 blew up"),
+			killer = NS("@1 was blown up by @2"),
+			item = NS("@1 was blown up by @2 using @3"),
 			-- "@1 was killed by [Intentional Game Design]" -- for exploding bed in nether or end
 		},
 		cramming = {
-			plain = W("@1 was squished too much"),
-			assist = W("@1 was squashed by @2"),	-- surprisingly "escape" is actually the correct subtype
+			plain = NS("@1 was squished too much"),
+			assist = NS("@1 was squashed by @2"),	-- surprisingly "escape" is actually the correct subtype
 		},
 		fireworks = {
-			plain = W("@1 went off with a bang"),
-			item = W("@1 went off with a bang due to a firework fired by @2 from @3"),
+			plain = NS("@1 went off with a bang"),
+			item = NS("@1 went off with a bang due to a firework fired by @2 from @3"),
 		},
 		sweet_berry = {
-			plain = W("@1 was poked to death by a sweet berry bush"),
-			assist = W("@1 was poked to death by a sweet berry bush whilst trying to escape @2"),
+			plain = NS("@1 was poked to death by a sweet berry bush"),
+			assist = NS("@1 was poked to death by a sweet berry bush whilst trying to escape @2"),
 		},
 		-- Missing snowballs: The Minecraft wiki mentions them but it doesn't seem like they have a dedicated death message in MC
 	},
@@ -141,13 +140,13 @@ local function get_item_killer_message(obj, messages, reason)
 			if mcl_enchanting.is_enchanted(wielded:get_name()) then
 				itemname = minetest.colorize(mcl_colors.AQUA, itemname)
 			end
-			return messages.item(mcl_util.get_object_name(obj), mcl_util.get_object_name(reason.source), itemname)
+			return S(messages.item, mcl_util.get_object_name(obj), mcl_util.get_object_name(reason.source), itemname)
 		end
 	end
 end
 
 local function get_plain_killer_message(obj, messages, reason)
-	return messages.killer and messages.killer(mcl_util.get_object_name(obj), mcl_util.get_object_name(reason.source))
+	return messages.killer and S(messages.killer, mcl_util.get_object_name(obj), mcl_util.get_object_name(reason.source))
 end
 
 local function get_killer_message(obj, messages, reason)
@@ -156,13 +155,13 @@ end
 
 local function get_assist_message(obj, messages, reason)
 	if messages.assist and mcl_death_messages.assist[obj] then
-		return messages.assist(mcl_util.get_object_name(obj), mcl_death_messages.assist[obj].name)
+		return S(messages.assist, mcl_util.get_object_name(obj), mcl_death_messages.assist[obj].name)
 	end
 end
 
 local function get_plain_message(obj, messages, reason)
 	if messages.plain then
-		return messages.plain(mcl_util.get_object_name(obj))
+		return S(messages.plain, mcl_util.get_object_name(obj))
 	end
 end
 
