0dd1275d867226ab6725bd07607ffe8003a304fe
Elias Åström <ryvnf@riseup.net>
2024-01-21 13:46:42 +0100

    Revert "Only allow to move amount of items that fit in target into sorter list"
    This reverts commit 5748ae0624665042ada918b23ca3869e21ab8099.

--- mods/ITEMS/mcl_furnaces/init.lua
+++ mods/ITEMS/mcl_furnaces/init.lua
@@ -114,13 +114,6 @@ end
 -- Node callback functions that are the same for active and inactive furnace
 --
 
-local function sort_stack(stack)
-	if mcl_util.is_fuel(stack) then
-		return "fuel"
-	end
-	return "src"
-end
-
 function mcl_furnaces.allow_metadata_inventory_put(pos, listname, index, stack, player)
 	local name = player:get_player_name()
 	if minetest.is_protected(pos, name) then
@@ -165,16 +158,7 @@ function mcl_furnaces.allow_metadata_inventory_put(pos, listname, index, stack, 
 	elseif listname == "dst" then
 		return 0
 	elseif listname == "sorter" then
-		local inv = minetest.get_meta(pos):get_inventory()
-		local trg = sort_stack(stack)
-		local stack1 = ItemStack(stack):take_item()
-		if inv:room_for_item(trg, stack) then
-			return stack:get_count()
-		elseif inv:room_for_item(trg, stack1) then
-			local tc = inv:get_stack(trg, 1):get_count()
-			return stack:get_stack_max() - tc
-		end
-		return 0
+		return stack:get_count()
 	end
 end
 
@@ -217,7 +201,16 @@ end
 function mcl_furnaces.on_metadata_inventory_put(pos, listname, index, stack, player)
 	if listname == "sorter" then
 		local inv = minetest.get_meta(pos):get_inventory()
-		inv:add_item(sort_stack(stack), stack)
+		local rest
+		if mcl_util.is_fuel(stack) then
+			rest = inv:add_item("fuel", stack)
+		else
+			rest = inv:add_item("src", stack)
+		end
+		if rest and rest:get_count() > 0 then
+			local pinv = player:get_inventory()
+			pinv:add_item("main", rest)
+		end
 		inv:set_stack("sorter", 1, ItemStack(""))
 	end
 end
