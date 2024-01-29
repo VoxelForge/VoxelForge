ce4c49e7c74d248a53f528ea41f623776b29360d
codiac <codiac@inbox.lv>
2024-01-29 10:07:39 +1000

    rename texture to avoid conflict

--- mods/MAPGEN/mcl_villages/textures/src/gen_images.sh
+++ mods/MAPGEN/mcl_villages/textures/src/gen_images.sh
@@ -13,7 +13,7 @@ for  (( i = 1; i <= ${#tiles[@]}; i++ )); do
     tile=${tiles[$i - 1]}
 	composite ../../../ITEMS/mcl_farming/textures/mcl_farming_wheat_stage_7.png ${tile} grain_${i}.png;
 	composite ../../../ITEMS/mcl_farming/textures/farming_carrot.png ${tile} root_${i}.png;
-	composite src/farming_pumpkin_side.png  ${tile} gourd_${i}.png;
+	composite src/farming_pumpkin_side_small.png  ${tile} gourd_${i}.png;
 	composite ../../../ITEMS/mcl_farming/textures/mcl_farming_sweet_berry_bush_0.png  ${tile} bush_${i}.png;
 	composite ../../../ITEMS/mcl_flowers/textures/flowers_tulip.png  ${tile} flower_${i}.png;
 	composite ../../../ITEMS/mcl_core/textures/default_sapling.png  ${tile} tree_${i}.png;
