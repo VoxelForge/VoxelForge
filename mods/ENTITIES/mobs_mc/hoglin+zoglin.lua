e8b72a99a76d49ec920b74e45233bfb4f20e56fb
nixnoxus <nixnoxus@web.de>
2024-02-25 17:56:08 +0100

    change `runaway_from` to `avoid_from`

--- mods/ENTITIES/mobs_mc/hoglin+zoglin.lua
+++ mods/ENTITIES/mobs_mc/hoglin+zoglin.lua
@@ -43,10 +43,10 @@ local hoglin = {
 	run_velocity = 1.3, -- ( was 2.8 )  < 2.4 is slow and 2.6 < is fast
 	retaliates = true,
 	group_attack = true,
-
-	runaway_from = {
+	avoid_from = {
 		"mcl_crimson:warped_fungus",
 		"mcl_flowerpots:flower_pot_warped_fungus",
+		"mcl_portals:portal",
 		"mcl_beds:respawn_anchor",
 		"mcl_beds:respawn_anchor_charged_1",
 		"mcl_beds:respawn_anchor_charged_2",
@@ -106,18 +106,7 @@ local hoglin = {
 	attack_animals = true,
 }
 mcl_mobs.register_mob("mobs_mc:hoglin", hoglin)
---[[
-hoglin.on_breed = function(parent1, parent2)
-	local pos = parent1.object:get_pos()
-	local child = mcl_mobs.spawn_child(pos, parent1.name)
-	if child then
-		local ent_c = child:get_luaentity()
-		-- ent_c.tamed = true
-		ent_c.owner = parent1.owner
-		return false
-	end
-end
---]]
+
 mcl_mobs.register_mob("mobs_mc:zoglin",table.merge(hoglin,{
 	description = S("Zoglin"),
 	fire_resistant = 1,
