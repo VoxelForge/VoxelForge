4f40db56019be3e8801f097defcc39a26da679e8
nixnoxus <nixnoxus@web.de>
2024-03-28 21:41:05 +0100

    Add the growth of `weeping_vines` and `twisting_vines`

--- mods/ITEMS/mcl_crimson/init.lua
+++ mods/ITEMS/mcl_crimson/init.lua
@@ -47,17 +47,38 @@ function generate_crimson_tree(pos)
 	minetest.place_schematic(pos,modpath.."/schematics/crimson_fungus_1.mts","random",nil,false,"place_center_x,place_center_z")
 end
 
-function grow_vines(pos, amount ,vine, dir)
-	if dir == nil then dir = 1 end
-	local tip = mcl_util.traverse_tower(pos, dir)
-	for i=1,amount do
+local max_vines_age = 25
+local grow_vines_direction = { ["mcl_crimson:twisting_vines"] = 1, ["mcl_crimson:weeping_vines"] = -1 }
+
+function set_vines_age(pos, node)
+	local dir = grow_vines_direction[node.name]
+	local vpos, i = mcl_util.traverse_tower(pos, -dir)
+	for i = 1, i do
+		minetest.swap_node(vpos, { name = node.name, param2 = i })
+		vpos = vector.offset(vpos, 0, dir, 0)
+	end
+	return i
+end
+
+function get_vines_age(pos)
+	local node = minetest.get_node(pos)
+	return node.param2 > 0 and node.param2 or set_vines_age(pos, node)
+end
+
+function grow_vines(pos, amount, vine, dir, max_age)
+	dir = dir or grow_vines_direction[vine] or 1
+	local tip, i = mcl_util.traverse_tower(pos, dir)
+	local age = get_vines_age(pos) + i -1
+	amount = math.min(amount, max_age and max_age - age or amount)
+	for i=1, amount do
 		local p = vector.offset(tip,0,dir*i,0)
 		if minetest.get_node(p).name == "air" then
-			minetest.set_node(p,{name=vine})
+			minetest.set_node(p,{name=vine, param2=age +i})
 		else
-			break
+			return i-1
 		end
 	end
+	return amount
 end
 
 local nether_wood_groups = { handy = 1, axey = 1, material_wood = 1, }
@@ -166,7 +187,6 @@ minetest.register_node("mcl_crimson:warped_fungus", {
 	groups = {dig_immediate=3,mushroom=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1,enderman_takable=1,deco_block=1,compostability=65},
 	light_source = 1,
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
-	node_placement_prediction = "",
 	on_rightclick = function(pos, node, pointed_thing, player, itemstack)
 		if pointed_thing:get_wielded_item():get_name() == "mcl_bone_meal:bone_meal" then
 			local nodepos = minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z})
@@ -188,161 +208,100 @@ mcl_flowerpots.register_potted_flower("mcl_crimson:warped_fungus", {
 	image = "farming_warped_fungus.png",
 })
 
-minetest.register_node("mcl_crimson:twisting_vines", {
+local call_on_place = function(itemstack, placer, pointed_thing)
+	local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
+	if rc then return rc end
+
+	local dir = vector.direction(pointed_thing.under, pointed_thing.above).y
+	local node = minetest.get_node(pointed_thing.under)
+	local idef = itemstack:get_definition()
+
+	local pos = vector.offset(pointed_thing.under, 0, grow_vines_direction[idef.name], 0)
+	if mcl_util.check_position_protection(pos, placer) then return itemstack end
+
+	if node.name == idef.name then
+		if grow_vines(pointed_thing.under, 1, node.name) == 0 then return end
+	elseif grow_vines_direction[idef.name] == dir and minetest.get_item_group(node.name, "solid") ~= 0 then
+		minetest.item_place_node(itemstack, placer, pointed_thing, 1)
+	else
+		return itemstack
+	end
+
+	if idef.sounds and idef.sounds.place then
+		minetest.sound_play(idef.sounds.place, {pos=pointed_thing.above, gain=1}, true)
+	end
+	if not minetest.is_creative_enabled(placer:get_player_name()) then
+		itemstack:take_item(1)
+	end
+	return itemstack
+end
+
+local call_on_dig = function(pos, node, digger)
+	local dir = grow_vines_direction[node.name]
+	local descendent = vector.offset(pos,0,dir,0)
+	minetest.node_dig(pos, node, digger)
+	if minetest.get_node(descendent).name ~= node.name then return end
+	mcl_util.traverse_tower(descendent, dir, function(p, dir, n)
+		minetest.node_dig(p, n, digger)
+	end)
+end
+
+local function register_vines(name, def)
+	minetest.register_node(name, table.merge({
+		drawtype = "plantlike",
+		sunlight_propagates = true,
+		paramtype = "light",
+		walkable = false,
+		climbable = true,
+		buildable_to = true,
+		groups = {dig_immediate=3, shearsy=1, vines=1, dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1, deco_block=1, compostability=50},
+		sounds = mcl_sounds.node_sound_leaves_defaults(),
+		node_placement_prediction = "",
+		on_place = call_on_place,
+		on_dig = call_on_dig,
+		drop = {
+			max_items = 1,
+			items = {
+				{items = {name}, rarity = 3},
+			},
+		},
+		_mcl_shears_drop = true,
+		_mcl_silk_touch_drop = true,
+		_mcl_fortune_drop = {
+			items = {
+				{items = {name}, rarity = 3},
+				{items = {name}, rarity = 1.8181818181818181},
+			},
+			name,
+			name,
+		},
+		_mcl_blast_resistance = 0.2,
+		_mcl_hardness = 0.2,
+		_on_bone_meal = function(itemstack,placer,pointed_thing,pos,node)
+			grow_vines(pos, math.random(1, 3), name, nil, max_vines_age)
+		end
+	}, def or {}))
+end
+
+register_vines("mcl_crimson:twisting_vines", {
 	description = S("Twisting Vines"),
-	drawtype = "plantlike",
 	tiles = { "twisting_vines_plant.png" },
 	inventory_image = "twisting_vines.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	climbable = true,
-	buildable_to = true,
-	groups = {dig_immediate=3, shearsy=1, vines=1, dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1, deco_block=1, compostability=50},
-	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	selection_box = {
 		type = "fixed",
 		fixed = { -3/16, -0.5, -3/16, 3/16, 0.5, 3/16 },
 	},
-	node_placement_prediction = "",
-	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-		local pn = clicker:get_player_name()
-		if clicker:is_player() and minetest.is_protected(vector.offset(pos,0,1,0), pn or "") then
-			minetest.record_protection_violation(vector.offset(pos,0,1,0), pn)
-			return itemstack
-		end
-		if clicker:get_wielded_item():get_name() == "mcl_crimson:twisting_vines" then
-			grow_vines(pos, 1, "mcl_crimson:twisting_vines")
-			local idef = itemstack:get_definition()
-			local _, success = minetest.item_place_node(itemstack, clicker, pointed_thing)
-			if success then
-			if idef.sounds and idef.sounds.place then
-				minetest.sound_play(idef.sounds.place, {pos=pointed_thing.above, gain=1}, true)
-			end
-		end
-
-		elseif clicker:get_wielded_item():get_name() == "mcl_bone_meal:bone_meal" then
-			if not minetest.is_creative_enabled(clicker:get_player_name()) then
-				itemstack:take_item()
-			end
-			grow_vines(pos, math.random(1, 3),"mcl_crimson:twisting_vines")
-		end
-		return itemstack
-	end,
-	on_place = function(itemstack, placer, pointed_thing)
-		local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
-		if rc then return rc end
-		if pointed_thing.under.y < pointed_thing.above.y then
-			if minetest.get_item_group( minetest.get_node(pointed_thing.under).name, "solid") == 0 or minetest.is_protected(pointed_thing.above, placer:get_player_name()) then
-				return
-			end
-			minetest.item_place_node(itemstack, placer, pointed_thing)
-			if not minetest.is_creative_enabled(placer:get_player_name()) then
-				return itemstack
-			end
-		end
-	end,
-	on_dig = function(pos, node, digger)
-		local above = vector.offset(pos,0,1,0)
-		minetest.node_dig(pos, node, digger)
-		if minetest.get_node(above).name ~= node.name then return end
-		mcl_util.traverse_tower(above,1,function(p, dir, n)
-			minetest.node_dig(p, n, digger)
-		end)
-	end,
-
-	drop = {
-		max_items = 1,
-		items = {
-			{items = {"mcl_crimson:twisting_vines"}, rarity = 3},
-		},
-	},
-	_mcl_shears_drop = true,
-	_mcl_silk_touch_drop = true,
-	_mcl_fortune_drop = {
-		items = {
-			{items = {"mcl_crimson:twisting_vines"}, rarity = 3},
-			{items = {"mcl_crimson:twisting_vines"}, rarity = 1.8181818181818181},
-		},
-		"mcl_crimson:twisting_vines",
-		"mcl_crimson:twisting_vines",
-	},
-	_mcl_blast_resistance = 0.2,
-	_mcl_hardness = 0.2,
 })
 
-minetest.register_node("mcl_crimson:weeping_vines", {
+register_vines("mcl_crimson:weeping_vines", {
 	description = S("Weeping Vines"),
-	drawtype = "plantlike",
 	tiles = { "mcl_crimson_weeping_vines.png" },
 	inventory_image = "mcl_crimson_weeping_vines.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	climbable = true,
-	buildable_to = true,
-	groups = {dig_immediate=3, shearsy=1, vines=1, dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1, deco_block=1, compostability=50},
-	sounds = mcl_sounds.node_sound_leaves_defaults(),
+
 	selection_box = {
 		type = "fixed",
 		fixed = { -3/16, -0.5, -3/16, 3/16, 0.5, 3/16 },
 	},
-	node_placement_prediction = "",
-	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-		local pn = clicker:get_player_name()
-		if clicker:is_player() and minetest.is_protected(vector.offset(pos,0,1,0), pn or "") then
-			minetest.record_protection_violation(vector.offset(pos,0,1,0), pn)
-			return itemstack
-		end
-		if clicker:get_wielded_item():get_name() == "mcl_crimson:weeping_vines" then
-			grow_vines(pos, 1, "mcl_crimson:weeping_vines", -1)
-			local idef = itemstack:get_definition()
-			local _, success = minetest.item_place_node(itemstack, clicker, pointed_thing)
-			if success then
-			if idef.sounds and idef.sounds.place then
-				minetest.sound_play(idef.sounds.place, {pos=pointed_thing.above, gain=1}, true)
-			end
-		end
-		elseif clicker:get_wielded_item():get_name() == "mcl_bone_meal:bone_meal" then
-			if not minetest.is_creative_enabled(clicker:get_player_name()) then
-				itemstack:take_item()
-			end
-			grow_vines(pos, math.random(1, 3),"mcl_crimson:weeping_vines", -1)
-		end
-		return itemstack
-	end,
-	on_dig = mcl_core.dig_vines,
-	on_place = function(itemstack, placer, pointed_thing)
-		local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
-		if rc then return rc end
-		if pointed_thing.under.y > pointed_thing.above.y then
-			if minetest.get_item_group( minetest.get_node(pointed_thing.under).name, "solid") == 0 or minetest.is_protected(pointed_thing.above, placer:get_player_name()) then
-				return
-			end
-			minetest.item_place_node(itemstack, placer, pointed_thing)
-			if not minetest.is_creative_enabled(placer:get_player_name()) then
-				return itemstack
-			end
-		end
-	end,
-	drop = {
-		max_items = 1,
-		items = {
-			{items = {"mcl_crimson:weeping_vines"}, rarity = 3},
-		},
-	},
-	_mcl_shears_drop = true,
-	_mcl_silk_touch_drop = true,
-	_mcl_fortune_drop = {
-		items = {
-			{items = {"mcl_crimson:weeping_vines"}, rarity = 3},
-			{items = {"mcl_crimson:weeping_vines"}, rarity = 1.8181818181818181},
-		},
-		"mcl_crimson:weeping_vines",
-		"mcl_crimson:weeping_vines",
-	},
-	_mcl_blast_resistance = 0.2,
-	_mcl_hardness = 0.2,
 })
 
 minetest.register_node("mcl_crimson:nether_sprouts", {
@@ -531,6 +490,18 @@ minetest.register_abm({
 		if minetest.get_item_group(minetest.get_node(vector.offset(pos, 0, 1, 0)).name, "solid") > 0 then
 			node.name = "mcl_nether:netherrack"
 			minetest.set_node(pos, node)
+		end
+	end
+})
+
+minetest.register_abm({
+	label = "Weeping Vines and Twisting Vines growth",
+	nodenames = { "mcl_crimson:weeping_vines", "mcl_crimson:twisting_vines" },
+	interval = 47 * 2.5,
+	chance = 4,
+	action = function(pos, node)
+		if node.param2 < max_vines_age then
+			grow_vines(pos, 1, node.name, nil, max_vines_age)
 		end
 	end
 })
