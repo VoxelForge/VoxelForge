a82c3c091dca596704a4a9d7d8d55a2e714898cc
cora <coradelamouche@gmx.ch>
2024-04-14 22:24:58 +0200

    Add buried treasure

--- mods/MAPGEN/mcl_structures/shipwrecks.lua
+++ mods/MAPGEN/mcl_structures/shipwrecks.lua
@@ -76,23 +76,59 @@ local ocean_biomes = {
 	"JungleM_ocean"
 }
 
---[[
-local beach_biomes = {
-	"FlowerForest_beach",
-	"Forest_beach",
-	"StoneBeach",
-	"ColdTaiga_beach_water",
-	"Taiga_beach",
-	"Savanna_beach",
-	"Plains_beach",
-	"ExtremeHills_beach",
-	"ColdTaiga_beach",
-	"Swampland_shore",
-	"MushroomIslandShore",
-	"JungleM_shore",
-	"Jungle_shore"
+local buried_treasure = {
+	{
+		stacks_min = 1,
+		stacks_max = 1,
+		items = {
+			{ itemstring = "mcl_mobitems:heart_of_the_sea", weight = 100, amount_min = 1, amount_max = 1 },
+		}
+	},
+	{
+		stacks_min = 5,
+		stacks_max = 8,
+		items = {
+			{ itemstring = "mcl_core:iron_ingot", weight = 20, amount_min = 1, amount_max = 4 },
+			{ itemstring = "mcl_core:gold_ingot", weight = 10, amount_min = 1, amount_max = 4 },
+			{ itemstring = "mcl_tnt:tnt", weight = 1, amount_min = 1, amount_max = 2 },
+		}
+	},
+	{
+		stacks_min = 2,
+		stacks_max = 2,
+		items = {
+			{ itemstring = "mcl_fishing:fish_cooked", weight = 1, amount_min = 2, amount_max = 4 },
+			{ itemstring = "mcl_fishing:salmon_cooked", weight = 1, amount_min = 2, amount_max = 4 },
+		}
+	},
+	{
+		stacks_min = 1,
+		stacks_max = 4,
+		items = {
+			{ itemstring = "mcl_core:emerald", weight = 5, amount_min = 4, amount_max = 8 },
+			{ itemstring = "mcl_ocean:prismarine_crystals", weight = 5, amount_min = 1, amount_max = 5 },
+			{ itemstring = "mcl_core:diamond", weight = 5, amount_min = 1, amount_max = 2 },
+		}
+	},
+	{
+		stacks_min = 0,
+		stacks_max = 1,
+		items = {
+			{ itemstring = "mcl_armor:chesplate_leather", weight = 1, amount_min = 1, amount_max = 1 },
+			{ itemstring = "mcl_tools:sword_iron", weight = 1, amount_min = 1, amount_max = 1 },
+		}
+	},
 }
---]] --might be used for buried treasure ..
+
+local function get_treasure_map(cpos)
+	local stack = ItemStack("mcl_books:written_book")
+	local bookmeta = stack:get_meta()
+	bookmeta:set_string("text", "There is a treasure at \n"..minetest.pos_to_string(cpos))
+	bookmeta:set_string("author", "The Albatross")
+	bookmeta:set_string("title", "Treasure")
+	bookmeta:set_string("description", "Treasure")
+	return stack
+end
 
 mcl_structures.register_structure("shipwreck",{
 	place_on = {"group:sand","mcl_core:gravel"},
@@ -114,6 +150,23 @@ mcl_structures.register_structure("shipwreck",{
 	y_min = mcl_vars.mg_overworld_min,
 	filenames = schems,
 	y_offset = function(pr) return pr:next(-4,-2) end,
+	after_place = function(p,def,pr)
+		local pos = vector.new(p.x, 1, p.z)
+		local sand = minetest.find_nodes_in_area_under_air(vector.offset(pos, -64, -2, -64), vector.offset(pos, 64, 5, 64), {"mcl_core:sand", "mcl_core:gravel", "mcl_core:dirt_with_grass", "mcl_core:mycelium", "mcl_core:podzol"})
+		local chests = minetest.find_nodes_in_area_under_air(vector.offset(pos, -8, -7, -8), vector.offset(pos, 8, 8, 8), {"mcl_chests:chest_small"})
+		if sand and #sand > 0 then
+			local ppos = sand[pr:next(1,#sand)]
+			local depth = pr:next(1,4)
+			local cpos = vector.offset(ppos, 0, -depth, 0)
+			minetest.set_node(cpos, {name = "mcl_chests:chest_small"})
+			minetest.registered_nodes["mcl_chests:chest_small"].on_construct(cpos)
+			mcl_loot.fill_inventory(minetest.get_meta(cpos):get_inventory(), "main", mcl_loot.get_multi_loot(buried_treasure, pr), pr)
+			if chests and #chests > 0 then
+				local cchest = chests[pr:next(1,#chests)]
+				minetest.get_meta(cchest):get_inventory():add_item("main", get_treasure_map(cpos))
+			end
+		end
+	end,
 	loot = {
 		["mcl_chests:chest_small"] = {
 			stacks_min = 3,
