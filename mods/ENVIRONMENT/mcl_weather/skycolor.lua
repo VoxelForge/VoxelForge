257162f519861b5bf5316bd50643102bf94a40b6
cora <coradelamouche@gmx.ch>
2024-04-16 23:48:16 +0200

    Set values for new player lighting properties

--- mods/ENVIRONMENT/mcl_weather/skycolor.lua
+++ mods/ENVIRONMENT/mcl_weather/skycolor.lua
@@ -364,7 +364,18 @@ end)
 
 local function initsky(player)
 	if player.set_lighting then
-		player:set_lighting({ shadows = { intensity = 0.33 } })
+		player:set_lighting({
+			shadows = { intensity = 0.33 },
+			volumetric_light = { strength = 0.45 },
+			exposure = {
+				luminance_min = -3.5,
+				luminance_max = -2.5,
+				exposure_correction = 0.35,
+				speed_dark_bright = 1500,
+				speed_bright_dark = 700,
+			},
+			saturation = 1.1,
+		})
 	end
 
 	if (mcl_weather.skycolor.active) then
