5cbe70675d35fc15c98e08376abcb75be1270e22
nixnoxus <nixnoxus@web.de>
2024-04-20 17:51:33 +0200

    `soul_speed` also increases speed on `soul_soil`

--- mods/ITEMS/mcl_nether/init.lua
+++ mods/ITEMS/mcl_nether/init.lua
@@ -162,20 +162,24 @@ minetest.register_node("mcl_nether:soul_sand", {
 })
 
 mcl_player.register_globalstep_slow(function(player, dtime)
-	-- Standing on soul sand? If so, walk slower (unless player wears Soul Speed boots)
-	if mcl_player.players[player].nodes.stand == "mcl_nether:soul_sand" then
+	-- Standing on soul sand or soul soil?
+	if minetest.get_item_group(mcl_player.players[player].nodes.stand, "soul_block") > 0 then
 		-- TODO: Tweak walk speed
 		-- TODO: Also slow down mobs
 		local boots = player:get_inventory():get_stack("armor", 5)
 		local soul_speed = mcl_enchanting.get_enchantment(boots, "soul_speed")
+		-- If player wears Soul Speed boots, increase speed
 		if soul_speed > 0 then
 			playerphysics.add_physics_factor(player, "speed", "mcl_playerplus:soul_sand", soul_speed * 0.105 + 1.3)
-		else
+		-- otherwise walk slower on soul sand
+		elseif mcl_player.players[player].nodes.stand == "mcl_nether:soul_sand" then
 			if mcl_player.players[player].nodes.stand_below == "mcl_core:ice" or  mcl_player.players[player].nodes.stand_below == "mcl_core:packed_ice" or  mcl_player.players[player].nodes.below == "mcl_core:slimeblock" or  mcl_player.players[player].nodes.stand_below == "mcl_core:water_source" then
 				playerphysics.add_physics_factor(player, "speed", "mcl_playerplus:soul_sand", 0.1)
 			else
 				playerphysics.add_physics_factor(player, "speed", "mcl_playerplus:soul_sand", 0.4)
 			end
+		else
+			playerphysics.remove_physics_factor(player, "speed", "mcl_playerplus:soul_sand")
 		end
 	else
 		playerphysics.remove_physics_factor(player, "speed", "mcl_playerplus:soul_sand")
