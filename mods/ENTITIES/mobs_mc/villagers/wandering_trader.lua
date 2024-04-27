f8796f86e8574f5eedbb04e7350c94ab9ef48479
cora <coradelamouche@gmx.ch>
2024-04-27 03:35:25 +0200

    Add trader spawn function

--- mods/ENTITIES/mobs_mc/villagers/wandering_trader.lua
+++ mods/ENTITIES/mobs_mc/villagers/wandering_trader.lua
@@ -1,5 +1,7 @@
 local modname = minetest.get_current_modname()
 local S = minetest.get_translator(modname)
+local spawn_interval = 20 * 60
+local current_chance = 25
 local wandering_trader = table.copy(mobs_mc.villager_mob)
 
 function wandering_trader:do_custom(dtime)
@@ -56,3 +58,48 @@ mcl_mobs.register_mob("mobs_mc:wandering_trader", wandering_trader)
 
 -- spawn eggs
 mcl_mobs.register_egg("mobs_mc:wandering_trader", S("Wandering Trader"), "#1E90FF", "#bc8b72", 0)
+
+function mobs_mc.spawn_trader_llama(pos)
+	local o = minetest.add_entity(pos, "mobs_mc:llama")
+	if o then
+		local ot = o:get_properties().textures
+		o:set_properties({
+			textures = {"blank.png", "mobs_mc_llama_decor_wandering_trader.png", ot[3]},
+		})
+	end
+end
+
+function mobs_mc.spawn_wandering_trader(pos)
+	minetest.add_entity(pos, "mobs_mc:wandering_trader")
+	for i=1,math.random(2) do
+		mobs_mc.spawn_trader_llama(pos)
+	end
+end
+
+minetest.register_chatcommand("spawn_wandering_trader", {
+	privs = { debug = true, },
+	func = function(pn, pr)
+		local pl = minetest.get_player_by_name(pn)
+		mobs_mc.spawn_wandering_trader(pl:get_pos())
+	end,
+})
+
+local function attempt_trader_spawn()
+	if math.random(100) < current_chance then
+		current_chance = 25
+		local ow_players = {}
+		for _, pl in pairs(minetest.get_connected_players()) do
+			if mcl_worlds.pos_to_dimension(pl:get_pos()) == "overworld" then
+				table.insert(ow_players, pl)
+			end
+		end
+		table.shuffle(ow_players)
+
+		--spawn_trader
+	else
+		current_chance = current_chance + 25
+	end
+	minetest.after(spawn_interval, attempt_trader_spawn)
+end
+
+minetest.after(spawn_interval, attempt_trader_spawn)
