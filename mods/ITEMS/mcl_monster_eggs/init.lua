edd446ce782e90e3edad1f897b3aec78a391a4b0
amino <amino@noreply.codeberg.org>
2024-04-28 13:58:02 +0200

    Use definitions from base node for infested variant.
    Also fix hardness and blast resistance values.

--- mods/ITEMS/mcl_monster_eggs/init.lua
+++ mods/ITEMS/mcl_monster_eggs/init.lua
@@ -5,38 +5,35 @@ local S = minetest.get_translator(minetest.get_current_modname())
 
 
 -- Template function for registering monster egg blocks
-local function register_block(subname, description, tiles, is_ground_content)
-	if is_ground_content == nil then
-		is_ground_content = false
+local function register_block(name, description)
+
+	local def = table.copy(minetest.registered_nodes[name])
+
+	def.description = description
+	table.update(def.groups, {spawns_silverfish = 1})
+	def.drop = ""
+	def.after_dig_node = function (pos, oldnode, oldmetadata, digger)
+		if not minetest.is_creative_enabled("") then
+			minetest.add_entity(pos, "mobs_mc:silverfish")
+		end
 	end
-	minetest.register_node("mcl_monster_eggs:monster_egg_"..subname, {
-		description = description,
-		tiles = tiles,
-		is_ground_content = is_ground_content,
-		groups = {dig_immediate = 3, spawns_silverfish = 1, deco_block = 1},
-		drop = "",
-		sounds = mcl_sounds.node_sound_stone_defaults(),
-		after_dig_node = function (pos, oldnode, oldmetadata, digger)
-			if not minetest.is_creative_enabled("") then
-				minetest.add_entity(pos, "mobs_mc:silverfish")
-			end
-		end,
-		_tt_help = S("Hides a silverfish"),
-		_doc_items_longdesc = S([[
-			An infested block is a block from which a silverfish will pop out when it is broken.
-			It looks identical to its normal counterpart.
-		]]),
-		_mcl_hardness = 0,
-		_mcl_blast_resistance = 0.5,
-	})
+	def._tt_help = S("Hides a silverfish")
+	def._doc_items_longdesc = S([[
+		An infested block is a block from which a silverfish will pop out when it is broken.
+		It looks identical to its normal counterpart.
+	]])
+	def._mcl_hardness = def._mcl_hardness / 2
+	def._mcl_blast_resistance = 0.75
+
+	local base = name:gsub("^[_%w]*:", "")
+
+	minetest.register_node("mcl_monster_eggs:monster_egg_"..base, def)
 end
 
 -- Register all the monster egg blocks
-register_block("stone", S("Infested Stone"), {"default_stone.png"}, true)
-register_block("cobble", S("Infested Cobblestone"), {"default_cobble.png"})
-register_block("stonebrick", S("Infested Stone Bricks"), {"default_stone_brick.png"})
-register_block("stonebrickcracked", S("Infested Cracked Stone Bricks"), {"mcl_core_stonebrick_cracked.png"})
-register_block("stonebrickmossy", S("Infested Mossy Stone Bricks"), {"mcl_core_stonebrick_mossy.png"})
-register_block("stonebrickcarved", S("Infested Chiseled Stone Bricks"), {"mcl_core_stonebrick_carved.png"})
-
-
+register_block("mcl_core:stone", S("Infested Stone"))
+register_block("mcl_core:cobble", S("Infested Cobblestone"))
+register_block("mcl_core:stonebrick", S("Infested Stone Bricks"))
+register_block("mcl_core:stonebrickcracked", S("Infested Cracked Stone Bricks"))
+register_block("mcl_core:stonebrickmossy", S("Infested Mossy Stone Bricks"))
+register_block("mcl_core:stonebrickcarved", S("Infested Chiseled Stone Bricks"))
