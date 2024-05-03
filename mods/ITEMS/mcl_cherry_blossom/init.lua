e94aae3add5afd7c0feb56802f22fb10cf5d36dd
nixnoxus <nixnoxus@web.de>
2024-05-03 19:51:35 +0200

    fix cherry tree trunk position

--- mods/ITEMS/mcl_cherry_blossom/init.lua
+++ mods/ITEMS/mcl_cherry_blossom/init.lua
@@ -8,10 +8,10 @@ mcl_trees.register_wood("cherry_blossom",{
 	sign_color="#F29889",
 	tree_schems= {
 		{file=modpath.."/schematics/mcl_cherry_blossom_tree_1.mts"},
-		{file=modpath.."/schematics/mcl_cherry_blossom_tree_2.mts", offset = vector.new(0,0,1)},
+		{file=modpath.."/schematics/mcl_cherry_blossom_tree_2.mts"},
 		{file=modpath.."/schematics/mcl_cherry_blossom_tree_3.mts"},
 		{file=modpath.."/schematics/mcl_cherry_blossom_tree_beehive_1.mts"},
-		{file=modpath.."/schematics/mcl_cherry_blossom_tree_beehive_2.mts", offset = vector.new(0,0,1)},
+		{file=modpath.."/schematics/mcl_cherry_blossom_tree_beehive_2.mts"},
 		{file=modpath.."/schematics/mcl_cherry_blossom_tree_beehive_3.mts"},
 	},
 	tree = { tiles = {"mcl_cherry_blossom_log_top.png", "mcl_cherry_blossom_log_top.png","mcl_cherry_blossom_log.png"} },
