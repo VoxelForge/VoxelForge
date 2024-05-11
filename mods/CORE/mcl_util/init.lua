a1be95bdb1225a7121cf5cc31e58d8be23055cd7
cora <coradelamouche@gmx.ch>
2024-05-11 05:08:42 +0200

    Make sure object still exists when dealing damage

--- mods/CORE/mcl_util/init.lua
+++ mods/CORE/mcl_util/init.lua
@@ -643,7 +643,7 @@ function mcl_util.deal_damage(target, damage, mcl_reason)
 			local armorgroups = target:get_armor_groups()
 			if armorgroups and not armorgroups.immortal then
 				local puncher = mcl_reason and mcl_reason.direct or target
-				if puncher then
+				if puncher and puncher.get_pos and puncher:get_pos() then
 					target:punch(puncher, 1.0, {full_punch_interval = 1.0, damage_groups = {fleshy = damage}}, vector.direction(puncher:get_pos(), target:get_pos()), damage)
 				end
 			end
