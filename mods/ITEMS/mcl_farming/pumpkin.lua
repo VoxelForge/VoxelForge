e969173e83c049b01b15f14ebd54748e3601eab6
cora <coradelamouche@gmx.ch>
2024-05-27 00:13:18 +0200

    Fix potential crash when creating iron golem

--- mods/ITEMS/mcl_farming/pumpkin.lua
+++ mods/ITEMS/mcl_farming/pumpkin.lua
@@ -230,10 +230,10 @@ minetest.register_node("mcl_farming:pumpkin_face_light", {
 	tiles = {"farming_pumpkin_top.png", "farming_pumpkin_top.png", "farming_pumpkin_side.png", "farming_pumpkin_side.png", "farming_pumpkin_side.png", "farming_pumpkin_face_light.png"},
 	groups = {handy=1, axey=1, pumpkin=1, building_block=1, dig_by_piston=1 },
 	sounds = mcl_sounds.node_sound_wood_defaults(),
-	on_construct = function(pos)
+	after_place_node = function(pos, placer, itemstack, pointed_thing)
 		-- Attempt to spawn iron golem or snow golem
-		mobs_mc.check_iron_golem_summon(pos)
-		mobs_mc.check_snow_golem_summon(pos)
+		mobs_mc.check_iron_golem_summon(pos, placer)
+		mobs_mc.check_snow_golem_summon(pos, placer)
 	end,
 	on_rotate = on_rotate,
 	_mcl_blast_resistance = 1,
