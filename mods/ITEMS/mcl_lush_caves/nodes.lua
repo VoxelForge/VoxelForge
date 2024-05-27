bbe8450713b6bffdc4aaac649746c69d97cc618a
amino <amino@noreply.codeberg.org>
2024-05-27 13:57:27 +0200

    Cave vines should not convert to dirt on hoe use.

--- mods/ITEMS/mcl_lush_caves/nodes.lua
+++ mods/ITEMS/mcl_lush_caves/nodes.lua
@@ -162,7 +162,7 @@ minetest.register_node("mcl_lush_caves:cave_vines", {
 			{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5}
 		},
 	},
-	groups = { shearsy = 1, dig_immediate=3, plant=1, dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1, cultivatable=1, converts_to_moss = 1 },
+	groups = { shearsy = 1, dig_immediate=3, plant=1, dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1, converts_to_moss = 1 },
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
 	_mcl_blast_hardness = 0,
