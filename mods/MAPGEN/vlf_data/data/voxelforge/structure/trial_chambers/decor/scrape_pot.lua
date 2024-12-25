5a79770d1bc67fac8ab488adc6abfd5e137bcb6e
crispiebacon <joshua.b.newport@gmail.com>
2024-12-24 21:00:21 -0600

    remove unused files

--- mods/MAPGEN/vlf_data/data/voxelforge/structure/trial_chambers/decor/scrape_pot.lua
+++
@@ -1,9 +0,0 @@
-return {
-    size = {x = 1, y = 2, z = 1},
-    nodes = {
-        {metadata = {placement_priority="0",id="minecraft:jigsaw",pool="minecraft:empty",target="minecraft:empty",generate="true",final_state="minecraft:waxed_oxidized_cut_copper",name="minecraft:decor",joint="aligned",selection_priority="0",joint_type=""}, name = "voxelforge:jigsaw", pos = {x = 0, y = 0, z = 0}, param2 = 1},
-        {metadata = {pot_faces="return {\"scrape\",\"scrape\",\"scrape\",\"scrape\"}"}, name = "vlf_pottery_sherds:pot", pos = {x = 0, y = 1, z = 0}, param2 = 3},
-    },
-    entities = {},
-    probability = 1
-}
--- mods/MAPGEN/vlf_data/data/voxelforge/trial_chambers/decor/flow_pot.gamedata.gamedata
+++
@@ -1,9 +0,0 @@
-return {
-    size = {x = 1, y = 2, z = 1},
-    nodes = {
-        {metadata = {placement_priority="0",id="minecraft:jigsaw",pool="minecraft:empty",target="minecraft:empty",joint="aligned",selection_priority="0",final_state="minecraft:waxed_oxidized_cut_copper",name="minecraft:decor",generate="true"}, name = "voxelforge:jigsaw", pos = {x = 0, y = 0, z = 0}, param2 = 1},
-        {metadata = {pot_faces="return {\"flow\",\"flow\",\"flow\",\"flow\"}"}, name = "vlf_pottery_sherds:pot", pos = {x = 0, y = 1, z = 0}, param2 = 3},
-    },
-    entities = {},
-    probability = 1
-}
